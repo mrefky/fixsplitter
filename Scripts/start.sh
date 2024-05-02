395eb828cde62c69df65c17cf697a9839e06fe22
mrefky <mrefky@hotmail.com>
2024-05-03 01:33:56 +0300

    1

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,19 +1,5 @@
-sudo snap remove multipass --purge
-sudo rm -rf ~/snap/multipass
-sudo snap remove lxd --purge
-sudo rm -rf ~/snap/lxd
-sudo snap install multipass --classic
-sudo snap install lxd --classic
-sudo lxd init
-sudo multipass set local.driver=lxd
-sudo multipass set local.passphrase=foo
-sudo multipass authenticate foo
-sudo snap connect multipass:lxd lxd
-sleep 10
-multipass networks
-multipass launch --name vm01 --cpus 2 --memory 8G --disk 20G --network eno1
-multipass launch --name vm02 --cpus 2 --memory 8G --disk 20G --network eno1
-multipass launch --name vm03 --cpus 2 --memory 8G --disk 20G --network eno1
-
-
-multipass list
+sudo microk8s.start
+#microk8s kubectl port-forward -n kube-system service/kubernetes-dashboard 10443:443
+echo "Checking status"
+sudo microk8s status --wait-ready
+#sudo microk8s dashboard-proxy --node-ip
---
+++ Scripts/status.sh
@@ -0,0 +1,1 @@
+ watch -n1 microk8s.kubectl get po -A #-o wide
