bf5ee550ab9a3a1c45495ddd60935ceecf22b64a
mrefky <mrefky@hotmail.com>
2024-05-06 10:11:17 +0300

    1

--- install_kafka.sh
+++ install_kafka.sh
@@ -4,5 +4,6 @@ helm install kafka bitnami/kafka --version="22.1.3" \
  --set deleteTopicEnable=true \
  --set externalAccess.service.port=19092 \
  --set persistence.size=1Gi \
- --set persistence.dataLogDir.size=1Gi 
- #--set global.storageClass=mayastor #openebs-rwx 
+ --set persistence.dataLogDir.size=1Gi \
+ --set global.storageClass=microk8s-hostpath 
+
